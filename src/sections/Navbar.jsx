7ebc253fa336af195940989a9c99f6f2f794b99c
rahmanabdulquadir <rahmanaq777@gmail.com>
2024-10-06 22:58:31 +0600

    navbar styles changed

--- src/sections/Navbar.jsx
+++ src/sections/Navbar.jsx
@@ -22,9 +22,9 @@ const Navbar = () => {
   return (
     <header className="fixed top-0 left-0 right-0 z-50 bg-black/90">
     <div className="max-w-7xl mx-auto">
-      <div className="flex justify-between items-center py-5 mx-auto c-space">
+      <div className="flex justify-between items-center  mx-auto c-space">
         <a href="/" className="text-neutral-400 font-bold text-xl hover:text-white transition-colors">
-          Rahman_AQ
+          <img className="w-36 h-36" src="/assets/r.png" alt="" />
         </a>
 
         <button
