7366c6ad3bb6e1100d7dbf57d5e4b2f5829638bc
rahmanabdulquadir <rahmanaq777@gmail.com>
2024-10-09 14:18:34 +0600

    project updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
-import Hero from "./sections/Hero";
-import Navbar from "./sections/Navbar";
+import { Header } from "./sections/Header";
+import { HeroSection } from "./sections/HeroSection";
+
 
 
 function App() {
   return (
-    <main>
-      <Navbar />
-      <Hero />
+    <main className="w-full h-screen bg-zinc-800">
+      <Header/>
+      <HeroSection/>
     </main>
   );
 }
