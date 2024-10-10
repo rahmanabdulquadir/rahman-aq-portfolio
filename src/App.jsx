d9c043c5d89aeba5602dc4e2eab45d1513fd48c7
rahmanabdulquadir <rahmanaq777@gmail.com>
2024-10-10 18:58:40 +0600

    backgournd image attached

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
-import { Header } from "./sections/Header";
-import { HeroSection } from "./sections/HeroSection";
-
-
+import Navbar from "./sections/Navbar";
 
 function App() {
   return (
-    <main className="w-full h-screen bg-zinc-800">
-      <Header/>
-      <HeroSection/>
+    <main className="w-full h-screen bg-[url('./assets/images/noise.jpg')] bg-fixed  bg-cover ">
+      <div className="bg-zinc-900">
+        <Navbar />
+      </div>
     </main>
   );
 }
