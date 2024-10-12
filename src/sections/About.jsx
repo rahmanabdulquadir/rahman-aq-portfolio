ec9481fba3f2916a777a05570faddca4ec8cfd51
rahmanabdulquadir <rahmanaq777@gmail.com>
2024-10-12 22:59:07 +0600

    font color and about section fixed

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -2,39 +2,37 @@ import Button from "../components/Button";
 
 const About = () => {
   return (
-    <div className="" id="about">
-      <div
-        id="about"
-        className="space-x-y mt-4   flex flex-col lg:flex-row items-center"
-      >
-        <div className="flex-1 slate lg:w-2/4 w-full lg:text-start text-center">
-          <h2 className="text-5xl lg:text-start text-center text-[#ccd6f6] font-bold mb-12">
-            / about me <span className="slate w-3 h-10"></span>
-          </h2>
+    <div
+      id="about"
+      className="space-x-y  flex flex-col lg:flex-row items-center justify-center"
+    >
+      <div className="flex-1 slate lg:w-2/4 w-full lg:text-start text-center">
+        <h2 className="text-5xl lg:text-start text-center text-[#ccd6f6] font-bold mb-12">
+          / about me <span className="slate w-3 h-10"></span>
+        </h2>
 
-          <div>
-            <h3 className="text-4xl font-semibold mb-4">Front-end Developer</h3>
-            <p className="mb-6 text-2xl">
-              Hey there, I am Rahman Abdul Quadir, a web developer with a twist
-              of unconventional creativity.I have great interest in full-stack
-              development, artificial intelligence, human-computer interactions,
-              and everything in between. Apart from coding, I like to play
-              games, listen music,reading books.
-            </p>
+        <div>
+          <h3 className="text-4xl font-semibold mb-4">Front-end Developer</h3>
+          <p className="mb-6 w-[700px] text-2xl">
+            Hey there, I am Rahman Abdul Quadir, a web developer with a twist of
+            unconventional creativity.I have great interest in full-stack
+            development, artificial intelligence, human-computer interactions,
+            and everything in between. Apart from coding, I like to play games,
+            listen music,reading books.
+          </p>
 
-            <div className="flex flex-col lg:flex-row gap-y-8 lg:gap-y-0 gap-x-8 items-center justify-center lg:justify-start">
-              <Button>Contact Me</Button>
-              <Button>My Portfolio</Button>
-            </div>
+          <div className="flex flex-col lg:flex-row gap-y-8 lg:gap-y-0 gap-x-8 items-center justify-center lg:justify-start">
+            <Button>Contact Me</Button>
+            <Button>My Portfolio</Button>
           </div>
         </div>
-        <div className="lg:w-2/4 w-full flex justify-center mt-10 lg:mt-0">
-          <img
-            className="lg:w-[280px] lg:h-[280px] w-60 h-60 rounded-lg"
-            // src={image}
-            alt=""
-          />
-        </div>
+      </div>
+      <div className=" flex justify-center mt-10 lg:mt-0">
+        <img
+          className="lg:w-[280px] lg:h-[280px] w-60 h-60 rounded-lg"
+          // src={image}
+          alt=""
+        />
       </div>
     </div>
   );
--- src/sections/Home.jsx
+++ src/sections/Home.jsx
@@ -7,17 +7,17 @@ const Home = () => {
   return (
     <div
       id="home"
-      className={`space-x-y bg-[url(${bubble})]  flex flex-col items-center justify-center  text-white text-center p-2`}
+      className={`space-x-y relative  flex flex-col items-center justify-center  text-white text-center p-2`}
     >
-      <h1 className="text-4xl md:text-6xl font-bold mb-4 font-code">
-        hi, <span className="">Rahman</span> here!
+      <h1 className="text-4xl md:text-7xl font-bold mb-4 font-code">
+        hi, <span className="bg-gradient-to-r from-cyan-500 via-blue-500 to-purple-500 bg-clip-text text-transparent">Rahman</span> here!
       </h1>
       <div className="lg:w-[370px] w-[200px]">
         <img className="lg:ml-32 ml-20 mt-[-10px]" src={curve} alt="" />
       </div>
 
-      <p className="text-lg md:text-2xl mt-7 bg-gradient-to-r from-cyan-500 via-blue-500 to-purple-500 bg-clip-text text-transparent">I create stuff sometimes</p>
-      <p className="w-[550px] text-center">
+      <p className="text-xl md:text-3xl font-semibold mt-7 bg-gradient-to-r from-cyan-500 via-blue-500 to-purple-500 bg-clip-text text-transparent">I create stuff sometimes</p>
+      <p className="lg:w-[550px] text-center py-5 leading-loose">
         Howdy! I'm Nowrin. I graduated from North University of China in
         Computer Science and Technology. I'm passionate about Web development.
         Apart from coding, I like to play games, listen music,reading books.
@@ -27,7 +27,7 @@ const Home = () => {
           <a href="#" className=" font-code">Resume</a>
         </Button>
 
-        {/* <img src={bubble} alt=""  className="lg:mt-[-320px]"/> */}
+        <img src={bubble} alt=""  className="absolute z-[-20]"/>
 
     </div>
   );
