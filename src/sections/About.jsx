26e5ad5d8aad8ead66f851ec4a85179904b6d942
rahmanabdulquadir <rahmanaq777@gmail.com>
2024-10-12 23:35:39 +0600

    about section complete

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -4,16 +4,18 @@ const About = () => {
   return (
     <div
       id="about"
-      className="space-x-y  flex flex-col lg:flex-row items-center justify-center"
+      className="space-x-y  flex flex-col-reverse lg:flex-row items-center justify-center"
     >
-      <div className="flex-1 slate lg:w-2/4 w-full lg:text-start text-center">
-        <h2 className="text-5xl lg:text-start text-center text-[#ccd6f6] font-bold mb-12">
-          / about me <span className="slate w-3 h-10"></span>
+      <div className="flex-1 lg:w-2/4 w-full lg:text-start text-center">
+        <h2 className="hidden lg:block text-5xl lg:text-start text-center font-bold mb-16">
+          / about me
         </h2>
 
-        <div>
-          <h3 className="text-4xl font-semibold mb-4">Front-end Developer</h3>
-          <p className="mb-6 w-[700px] text-2xl">
+        <div className="px-6">
+          <h3 className="text-4xl font-semibold mb-4 bg-gradient-to-r from-cyan-500 via-blue-500 to-purple-500 bg-clip-text text-transparent">
+            Front-end Developer
+          </h3>
+          <p className="mb-6 lg:w-[700px] lg:text-start text-center text-lg lg:text-2xl leading-loose">
             Hey there, I am Rahman Abdul Quadir, a web developer with a twist of
             unconventional creativity.I have great interest in full-stack
             development, artificial intelligence, human-computer interactions,
@@ -27,9 +29,12 @@ const About = () => {
           </div>
         </div>
       </div>
-      <div className=" flex justify-center mt-10 lg:mt-0">
+      <div className=" flex flex-col justify-center mt-10 lg:mt-0">
+        <h2 className="lg:hidden block text-2xl lg:text-start text-center font-bold mb-16">
+          / about me
+        </h2>
         <img
-          className="lg:w-[280px] lg:h-[280px] w-60 h-60 rounded-lg"
+          className="lg:w-[280px] lg:h-[280px] w-60 h-60 rounded-xl pb-7"
           // src={image}
           alt=""
         />
