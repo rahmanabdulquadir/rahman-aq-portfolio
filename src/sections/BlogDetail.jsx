e7d7386992e1b0dcd8d9962ce588277c2462e8eb
rahmanabdulquadir <rahmanaq777@gmail.com>
2024-10-29 17:04:29 +0600

    updated

--- src/sections/BlogDetail.jsx
+++ src/sections/BlogDetail.jsx
@@ -1,9 +1,11 @@
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { BiArrowBack } from "react-icons/bi";
+import { useNavigate, useParams } from "react-router-dom";
 
 const BlogDetail = () => {
   const { _id } = useParams(); // Get the blog id from the route
   const [blog, setBlog] = useState(null);
+  const navigate = useNavigate()
 
   useEffect(() => {
     // Fetch the blog details by ID
@@ -20,6 +22,13 @@ const BlogDetail = () => {
 
   return (
     <div className="space-x-y h-screen text-center">
+      <div
+        onClick={() => navigate(-1)}
+        className="flex items-center gap-3 pb-5 cursor-pointer"
+      >
+        <BiArrowBack className="text-3xl" />
+        <h2 className="text-white">Go Back</h2>
+      </div>
       <h1 className="text-3xl font-bold mb-4">{blog?.data?.title}</h1>
       <img
         src={blog?.data?.image}
